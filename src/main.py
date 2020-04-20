7323e354d151d8a0a64ffb7873f37c1883a46cdc
Kseniya Valadzko <kseniya.valadzko@itibo.com>
2020-04-20 18:22:41 +0300

    moving from fastapi to flask

--- src/main.py
+++ src/main.py
@@ -1,11 +1,33 @@
-from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+from flask import Flask, jsonify, send_file
+from flask_restful import request
+from flask_cors import CORS
+from flask_jwt_simple import (
+    JWTManager, jwt_required, create_jwt, get_jwt_identity
+)
 
-app = FastAPI()
-app.add_middleware(CORSMiddleware, allow_origins=['*'])
+from waitress import serve
+
+import os, os.path, tempfile
+import logging
+from logging.handlers import RotatingFileHandler, QueueHandler, QueueListener
+from queue import Queue
+
+import time
+from datetime import datetime, timedelta
+
+import requests
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True, expose_headers='Content-Disposition')
+jwt = JWTManager(app)
+
+@app.route('/welcome')
+def export():
+    try:
+        return {'message': 'Hello World!'}
+    except Exception as e:
+        return 'Failed to get stats: ' + str(e), 400
 
 
-@app.get("/welcome")
-def root():
-    print('got request')
-    return {"message": "Hello World"}
+if __name__ == "__main__":
+    serve(app, port=5000)
